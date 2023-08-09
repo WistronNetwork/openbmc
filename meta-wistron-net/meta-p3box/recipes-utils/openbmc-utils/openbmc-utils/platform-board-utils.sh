7813225a2ff1aa31c4a6284d43b5c435c7280133
Neal_Chen <neal_chen@wistron.com>
2023-08-09 21:12:56 +0800

    meta-p3box: platform-board-utils.sh: Check file extension
    Check file extension in platform_do_fan_update
    
    Signed-off-by: Neal_Chen <neal_chen@wistron.com>

--- meta-wistron-net/meta-p3box/recipes-utils/openbmc-utils/openbmc-utils/platform-board-utils.sh
+++ meta-wistron-net/meta-p3box/recipes-utils/openbmc-utils/openbmc-utils/platform-board-utils.sh
@@ -8,6 +8,12 @@ source /usr/local/bin/i2c-utils.sh
 
 FANCPLD_SYSFS_DIR="$(i2c_device_sysfs_abspath 3-0016)/hwmon/hwmon*"
 FAN_MAX=6
+BMC_READY=GPIOD2
+BMC_READY_VALUE=1
+
+platform_boot_ready() {
+    bmc_ready "enable"
+}
 
 platform_fw_fru_list() {
     echo "bmc mb fan psu xcvr"
@@ -28,8 +34,7 @@ platform_fw_component_list() {
         "xcvr") echo "port1 port2 port3 port4 port5 port6 port7 port8 port9 port10
              port11 port12 port13 port14 port15 port16 port17 port18 port19 port20
              port21 port22 port23 port24 port25 port26 port27 port28 port29 port30
-             port31 port32
-        "
+             port31 port32"
         ;;
     esac
 }
@@ -50,6 +55,7 @@ platform_do_mb_update() {
             return "$FW_STATUS_NOT_SUPPORTED"
         fi
 
+        printf "\n"
         cpldupdate-i2c $bus $addr "$image"
         ret=$?
         if [ "$ret" -ne 0 ]; then
@@ -67,6 +73,7 @@ platform_do_fan_update() {
     local component=$1
     local image=$2
 
+    extension=${image##*.}
     if [ "$extension" = "jed" ]; then
         if [ "$component" = "cpld3" ]; then
             bus=4
@@ -75,6 +82,7 @@ platform_do_fan_update() {
             return "$FW_STATUS_NOT_SUPPORTED"
         fi
 
+        printf "\n"
         cpldupdate-i2c $bus $addr "$image"
         ret=$?
         if [ "$ret" -ne 0 ]; then
